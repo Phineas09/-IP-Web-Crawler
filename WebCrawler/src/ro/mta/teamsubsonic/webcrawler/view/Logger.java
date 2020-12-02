f154ecb1102f6f5537537e149af223572c7b5cff
flavius <pantucu.flavius@yahoo.com>
2020-12-02 19:25:51 +0200

    :art: Imbunatatire a structurii codului
    Issue: #12
    Imbunatatire a structurii codului si adaugarea exceptiilor.
    
    Signed-off-by: flavius <pantucu.flavius@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
@@ -1,59 +1,69 @@
 package ro.mta.teamsubsonic.webcrawler.view;
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions._CrawlerException;
+
+import java.io.*;
+
 /**
  * Class implementing the Logger part.
  * Implements constructor and write function for the program output
  *
  * @author Panțucu Flavius
  */
-class Logger
+public class Logger
 {
     /**
-     * Member decription
+     * Member description
      */
-    private static Logger instance = null;  //singleton object
-    public String         outputFile;       //where the output will be written
+    private static Logger instance = null;  // singleton object
+    public FileWriter     outputFile;       // the writer who will do the job done writing output
     /**
      * Logger class constructor
-     * @param _outputFile The file we work on
      */
     private Logger() { this.outputFile = null; }
-    private Logger(String _outputFile) { this.outputFile = _outputFile; }
 
-    public static Logger getInstance(String _output)
+    public static Logger getInstance()
     {
-        //verify if the singleton object is already created
+        // verify if the singleton object is already created
         if (instance == null)
-            //verify where we have to write program's output
-            if(_output == null)
                 instance = new Logger();
-            else
-                instance = new Logger(_output);
         return instance;
     }
-
     /**
      * Write output on the the file chosen at the beginning, stdout if null.
-     * @param outputText String that we have to display
+     * @param message string that we have to display
+     * @param filePath path to the file we need to write into
      */
-    public void write(String outputText){
-        if(this.outputFile == null){
-            System.out.println(outputText);
-        }
-        else{
-            Writer writer = null;
-            try {
-                writer = new BufferedWriter(new OutputStreamWriter(
-                        new FileOutputStream(this.outputFile), "utf-8"));
-                writer.write(outputText);
-            } catch (IOException ex) {
-            } finally {
-                try {writer.close();} catch (Exception ex) {}
+    public void write(String message, String filePath){
+        try {
+            if (filePath == null) {
+                throw new _CrawlerException("FileException","Invalid filepath entered");
+            } else {
+                File out = new File(filePath);
+
+                // creates the file
+                out.createNewFile();
+
+                // creates a FileWriter Object and write output
+                outputFile = new FileWriter(out);
+                outputFile.write(message);
+                outputFile.close();
             }
+        }
+        catch(_CrawlerException | IOException err) {
+            err.getMessage();
+        }
+    }
+    public void write(String message, int stdin){
+        try {
+           if(stdin == 0){
+               System.out.println(message);
+           }
+           else{
+               throw new _CrawlerException("InputException","Invalid value for stdin entered");
+           }
+        }
+        catch(_CrawlerException err) {
+            err.getMessage();
         }
     }
 }
