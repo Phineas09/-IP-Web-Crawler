0aeb21157dbaec4ed44a926775534d1cd0f4a845
flavius <pantucu.flavius@yahoo.com>
2020-12-02 13:06:40 +0200

    :construction: Implementează clasa Logger
    Issue: #1
    Implementează clasa Logger ce va fi folosită pentru afișarea output-ului aplicației
    Signed-off-by: flavius <pantucu.flavius@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
@@ -1,4 +1,42 @@
 package ro.mta.teamsubsonic.webcrawler.view;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class Logger {
+class Logger
+{
+
+    private static Logger instance = null;
+    public String         outputFile;
+
+    private Logger() { this.outputFile = null; }
+    private Logger(String _outputFile) { this.outputFile = _outputFile; }
+
+    public static Logger getInstance(String _output)
+    {
+        if (instance == null)
+            if(_output == null)
+                instance = new Logger();
+            else
+                instance = new Logger(_output);
+        return instance;
+    }
+    public void write(String outputText){
+        if(this.outputFile == null){
+            System.out.println(outputText);
+        }
+        else{
+            Writer writer = null;
+            try {
+                writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(this.outputFile), "utf-8"));
+                writer.write(outputText);
+            } catch (IOException ex) {
+            } finally {
+                try {writer.close();} catch (Exception ex) {}
+            }
+        }
+    }
 }
