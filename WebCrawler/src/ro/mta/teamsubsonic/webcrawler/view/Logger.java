1eceaaa5137347091dd2b2679b9c056defaa8639
flavius <pantucu.flavius@yahoo.com>
2020-12-02 14:38:00 +0200

    :art: Adaugare documentație cod
    Issue: #1
    Am adaugat codului scris detaliere.
    Signed-off-by: flavius <pantucu.flavius@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/view/Logger.java
@@ -4,25 +4,42 @@ import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+/**
+ * Class implementing the Logger part.
+ * Implements constructor and write function for the program output
+ *
+ * @author Panțucu Flavius
+ */
 class Logger
 {
-
-    private static Logger instance = null;
-    public String         outputFile;
-
+    /**
+     * Member decription
+     */
+    private static Logger instance = null;  //singleton object
+    public String         outputFile;       //where the output will be written
+    /**
+     * Logger class constructor
+     * @param _outputFile The file we work on
+     */
     private Logger() { this.outputFile = null; }
     private Logger(String _outputFile) { this.outputFile = _outputFile; }
 
     public static Logger getInstance(String _output)
     {
+        //verify if the singleton object is already created
         if (instance == null)
+            //verify where we have to write program's output
             if(_output == null)
                 instance = new Logger();
             else
                 instance = new Logger(_output);
         return instance;
     }
+
+    /**
+     * Write output on the the file chosen at the beginning, stdout if null.
+     * @param outputText String that we have to display
+     */
     public void write(String outputText){
         if(this.outputFile == null){
             System.out.println(outputText);
