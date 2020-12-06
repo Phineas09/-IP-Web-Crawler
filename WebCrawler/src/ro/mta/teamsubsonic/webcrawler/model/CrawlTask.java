3f10e0ff56017ca645f0f64ff96dc9c8366d1c5e
gunyx <gc.neacsu@yahoo.com>
2020-12-06 16:42:37 +0200

    :zap: Implement replaceRef method from CrawlTask class
    Issue: #7
    
    Am implementat metoda de inlocuire a referintelor paginii transmise prin url.
    Aceasta metoda este necesara localizarii locale a paginilor descarcate ce sunt referentiate de url-ul primit.
    
    Signed-off-by: gunyx <gc.neacsu@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
@@ -1,8 +1,20 @@
 package ro.mta.teamsubsonic.webcrawler.model;
 
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.FileException;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.InputException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Class that extends abstract class Task
@@ -13,7 +25,6 @@ import java.util.List;
  * @author Gunyx
  * @author Phineas09
  */
-
 
 public class CrawlTask extends Task {
     /**
@@ -50,8 +61,62 @@ public class CrawlTask extends Task {
     private void startTasks(){
 
     }
-    private void replaceHTMLRef(){
 
+    /**
+     * Method that replaces all references with the names of locally downloaded files
+     *
+     */
+    private void replaceHTMLRef() {
+        try {
+            String inPath = targetPath + "\\" + url;//aici cum numim fisierele-> NU putem pune url ca nume
+            String outPath = targetPath + "\\1" + url;
+
+            File oldFile = new File(inPath);
+            PrintWriter newFile = new PrintWriter(outPath);
+
+            Scanner reader = new Scanner(oldFile);
+
+            int nrRef = 0;
+            while (reader.hasNextLine()) {
+                int status = 0;
+                String data = reader.nextLine();
+                String patternString = "\\b((https?|ftp):\\/\\/)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[A-Za-z]{2,6}\\b(\\/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)*(?:\\/|\\b)";
+                Pattern pattern = Pattern.compile(patternString);
+                Matcher matcher = pattern.matcher(data);
+
+                String newStateLine = null;
+                String lastStateLine = data;
+                while (matcher.find()) {
+                    status = 1;
+                    nrRef++;
+
+                    String stringValue = String.valueOf(nrRef);
+                    int lenString = lastStateLine.length();
+
+                    newStateLine = lastStateLine.substring(0, matcher.start()) + stringValue + lastStateLine.substring(matcher.end(), lenString);
+                    matcher = pattern.matcher(newStateLine);
+                    lastStateLine = newStateLine;
+                }
+                if (status != 0) {
+                    newFile.println(newStateLine);
+                } else {
+                    newFile.println(data);
+                }
+            }
+            newFile.close();
+            reader.close();
+            oldFile.delete();
+
+            /**
+             * The new file is renamed after the input file
+             */
+            Path source = Paths.get(outPath);
+            Files.move(source, source.resolveSibling(inPath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
