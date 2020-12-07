344643487b662587a8cd0fc3db1eb3576f1833e6
Phineas09 <claudiu.ghenea15@yahoo.ro>
2020-12-07 11:32:00 +0200

    :memo: Completed methods
    Issue: #7
    
    Am implementat un schelet al functiilor pasePageHTML si downloadPage
    
    Co-authored-by: Phineas09 <claudiu.ghenea15@yahoo.ro>
    Co-authored-by: gunyx <gc.neacsu@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/WebCrawler.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/WebCrawler.java
@@ -1,8 +1,28 @@
 package ro.mta.teamsubsonic.webcrawler;
 
+import ro.mta.teamsubsonic.webcrawler.model.Configurations;
+import ro.mta.teamsubsonic.webcrawler.model.CrawlTask;
+import ro.mta.teamsubsonic.webcrawler.model.CrawlerThreadPool;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.CrawlerException;
+
 public class WebCrawler {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        try {
+            Configurations.getInstance(null);
+            CrawlerThreadPool crawlerThreadPool = CrawlerThreadPool.getInstance();
+
+            crawlerThreadPool.putTask(new CrawlTask(0, "https://github.com/Phineas09/-IP-Web-Crawler",
+                    "tests/mta.ro/index.html"));
+
+            while(crawlerThreadPool.threadSafeUpdatePoolCount(0) != 0) {
+                Thread.sleep(1000);
+            }
+            crawlerThreadPool.shutdownAndAwaitTermination();
+        }
+        catch (Exception exception) {
+            exception.getMessage();
+        }
     }
 }
--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
@@ -2,14 +2,15 @@ package ro.mta.teamsubsonic.webcrawler.model;
 
 import ro.mta.teamsubsonic.webcrawler.model.exceptions.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,31 +24,152 @@ import java.util.regex.Pattern;
  * @author Gunyx
  * @author Phineas09
  */
-
 public class CrawlTask extends Task {
+
     /**
      * Members of class description
      */
     private final int taskId;
     private final String url;
-    private final String targetPath;
-
+    private String targetPath;
+    private final String targetRoot;
 
     /**
      * Constructor of class
-     * @param taskId ->  which identifies the current task id.It is also used to identify the depth of recursion
-     * @param url ->contains the url from which the information is downloaded
-     * @param targetPath ->contains the path where the information is downloaded.
+     * @param taskId -> which identifies the current task id.It is also used to identify the depth of recursion
+     * @param url -> contains the url from which the information is downloaded
+     * @param targetRoot -> contains the path where the information is downloaded.
      */
-    public CrawlTask(int taskId, String url, String targetPath) {
+    public CrawlTask(int taskId, String url, String targetRoot) {
+        this.taskId = taskId;
+        this.url = url;
+        this.targetRoot = targetRoot;
+        this.targetPath = null;
+    }
+
+    /**
+     * Constructor of class
+     * @param taskId -> which identifies the current task id.It is also used to identify the depth of recursion
+     * @param url -> contains the url from which the information is downloaded
+     * @param targetRoot -> contains the path root where the information will be downloaded ( inherited from the previous task )
+     * @param targetPath -> contains the path where the information is downloaded.
+     */
+    public CrawlTask(int taskId, String url, String targetPath, String targetRoot) {
         this.taskId = taskId;
         this.url = url;
         this.targetPath = targetPath;
+        this.targetRoot = targetRoot;
     }
 
-    private HashMap<String,String> parsePageHtml() {
+    /**
+     * Parses a given web page for refs ( a | img | script | link ).
+     *
+     * @param webPage a web page in String format.
+     * @return A HashMap of links and destinations to be downloaded and replaced.
+     * @throws CrawlerException exception if parsing will fail.
+     */
+    private HashMap<String,String> parsePageHtml(String webPage) throws CrawlerException {
+        try {
+            String baseUrl = "https://" + (new URI(this.url)).getHost();
+            //Or make another variable to hold this information
 
-        return new HashMap<>();//referintele paginilor+locatia lor de download;
+            //Used for detecting all tags that may contain references
+            Pattern referencesPattern = Pattern.compile("(<(script|img|a|link)\\s+(?:[^>]*?\\s+)?" +
+                    "(src|href)(\\s?|\\s+)=(\\s?|\\s+)([\"'])(.*?)\\6)",
+                Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+            //Used for detecting paths from internal url
+                Pattern internalPattern = Pattern.compile("^\\.?(?:\\.{2})?(?:\\/\\.{2})*(\\/?[\\w\\d-\\.\\*]+)+",
+            //Pattern internalPattern = Pattern.compile("^\\.?(?:\\.{2})?(?:\\/\\.{2})*(\\/.+)+",
+                    Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+            HashMap<String, String> returnHash = new HashMap<>();
+
+            Matcher pageMatcher = referencesPattern.matcher(webPage);
+            ArrayList<String> links = new ArrayList<>();
+            while (pageMatcher.find()) {
+
+                String newUrl = pageMatcher.group(7);
+                if (newUrl.regionMatches(true, 0, baseUrl, 0, baseUrl.length())) {
+                    //If the reference is a website part of same domain
+                    links.add(newUrl);
+                }
+                else {
+                    Matcher pathMatcher = internalPattern.matcher(newUrl);
+                    if(pathMatcher.find() && newUrl.equals(pathMatcher.group())) {
+                        //If it is a reference inside the root dir
+                        newUrl = baseUrl + newUrl;
+                        //Check if is not .php
+
+                        //System.out.print("Internal path ");
+                    } else
+                        continue;
+                }
+
+                    String refType = pageMatcher.group(2);
+                    /*
+                    switch (refType) {
+                        case "script" -> System.out.print("Script ");
+                        case "img" -> System.out.print("Image ");
+                        case "a" -> System.out.print("Anchor ");
+                        case "link" -> System.out.print("Css or others ");
+                        default -> System.out.print("Error ");
+                    }
+*/
+
+                    //System.out.println(newUrl);
+
+                    //CrawlerThreadPool.getInstance().putTask(new CrawlTask(this.taskId + 1, newUrl, ));
+
+                //https://mta.ro/about-us | targetPath + "/page" + "/about-us.html"
+                //links.add(pageMatcher.group());
+
+            }
+            System.out.println(links.size());
+            for(var a : links) {
+                System.out.println(a);
+            }
+
+            return new HashMap<>();
+        }
+        catch (Exception exception) {
+            throw new InternalException("Web page does not exist!");
+        }
+    }
+
+    /**
+     * Downloads the web page of the current Task
+     *
+     * @return a string witch contains the entire web page
+     * @throws CrawlerException if an error occurs during the download process
+     */
+    private String downloadPage() throws CrawlerException {
+        try {
+            URL url = new URL(this.url);
+            BufferedReader siteReader =
+                    new BufferedReader(new InputStreamReader(url.openStream()));
+            // Read each line into a stringBuffer for further processing
+
+            File filePath = new File(targetPath.substring(0 , targetPath.lastIndexOf("/")));
+
+            if (!filePath.exists())
+                filePath.mkdirs();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(targetPath));
+            String line;
+            StringBuilder siteBuffer = new StringBuilder();
+            while ((line = siteReader.readLine()) != null) {
+                siteBuffer.append(line + "\n");
+                writer.write(line + "\n");
+            }
+
+            siteReader.close();
+            writer.close();
+            return siteBuffer.toString();
+        }
+        catch (Exception exception) {
+            throw new InternalException(exception.getMessage());
+        }
     }
 
     /**
@@ -78,10 +200,6 @@ public class CrawlTask extends Task {
         return false;
     }
 
-    private void downloadPage(){
-
-    }
-
     /**
      *The method that starts the tasks of downloading the initial url references to the max depth set
      * @param nextRef
@@ -99,10 +217,10 @@ public class CrawlTask extends Task {
                     Factory factoryTask = new Factory();
 
                     List<String> argsList = new ArrayList<>();
-                    argsList.add(element.getKey());
-                    argsList.add(element.getValue());
                     String idStr = String.valueOf(taskId + 1);
                     argsList.add((idStr));
+                    argsList.add(element.getKey());
+                    argsList.add(element.getValue());
 
                     Task downloadTask = factoryTask.createTask(CrawlTask.class, argsList);//tipul+lista de argumente
 
@@ -114,6 +232,8 @@ public class CrawlTask extends Task {
             error.getMessage();
         }catch (InputException error){
             error.getMessage();
+        } catch (CrawlerException crawlerException) {
+            crawlerException.getMessage();
         }
     }
 
@@ -179,9 +299,24 @@ public class CrawlTask extends Task {
      */
     @Override
     protected void _run() {
-        startTasks(parsePageHtml());
-        downloadPage();
-        replaceHTMLRef();
-
+        try {
+            //Only for the first task
+            if(this.taskId == 0) {
+                this.targetPath = this.targetRoot + "/index.html";
+            }
+            //startTasks(parsePageHtml());
+            parsePageHtml(downloadPage());
+            //replaceHTMLRef();
+            /**
+             *
+             * Download page -> return string + scrie in fisier pagina index.html practic
+             * ParsePageHTML -> HasMap(<URL target><Target path>)
+             * StartTasks -> Start all tasks to be downloaded  + verificare de depth si existenta ?
+             * Replace : <URL target> cu <Target path> with modified HashMap<String, String>
+             */
+        }
+        catch (CrawlerException crawlerException) {
+            crawlerException.getMessage();
+        }
     }
 }
