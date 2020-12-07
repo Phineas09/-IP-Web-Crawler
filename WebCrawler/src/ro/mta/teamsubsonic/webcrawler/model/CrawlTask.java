82a60a841e7ee681cd5661672a83a828bf11d8fc
gunyx <gc.neacsu@yahoo.com>
2020-12-07 10:16:58 +0200

    :sparkles: Implement some-methods in CrawlTask class
    Issue: #7
    
    Am implementat metodele
    1)startTasks care creaza si porneste recursiv task-urile pentru descarcarea recursiva a referintelor url-ului initial.
    2)testTask care testeaza daca task-ul curent depaseste maximul setat de recursivitate
    3)replaceRef care inlocuieste referintele url-ului initial cu path-urile locale unde sunt descarcate.
    
    Signed-off-by: gunyx <gc.neacsu@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/CrawlTask.java
@@ -1,8 +1,18 @@
 package ro.mta.teamsubsonic.webcrawler.model;
 
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.*;
 
-import java.util.HashMap;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Class that extends abstract class Task
@@ -13,7 +23,6 @@ import java.util.List;
  * @author Gunyx
  * @author Phineas09
  */
-
 
 public class CrawlTask extends Task {
     /**
@@ -40,18 +49,128 @@ public class CrawlTask extends Task {
 
         return new HashMap<>();//referintele paginilor+locatia lor de download;
     }
-    private boolean testTask(){
 
+    /**
+     * Boolean method that tests->if the maximum level of recursion has been reached
+     *                          ->if path and url is null
+     * @return true/false
+     */
+    private boolean testTask() {
+        try {
+            Configurations confInstance = Configurations.getInstance();
+
+            int depthLv=confInstance.getDepthLevel();
+            if((url==null)||(targetPath==null))
+            {
+                throw new InputException("Null HashMap given to method startTasks");
+            }
+            if(taskId+1<=depthLv)
+            {
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        } catch (_CrawlerException error){
+            error.getMessage();
+        }
         return false;
     }
+
     private void downloadPage(){
 
     }
-    private void startTasks(){
 
+    /**
+     *The method that starts the tasks of downloading the initial url references to the max depth set
+     * @param nextRef
+     */
+    private void startTasks(HashMap<String,String> nextRef){
+        try {
+            if(testTask()) {
+                CrawlerThreadPool threadPoolInstance = CrawlerThreadPool.getInstance();
+
+                if(nextRef==null)
+                {
+                    throw new InputException("Null HashMap given to method startTasks");
+                }
+                for (HashMap.Entry<String, String> element : nextRef.entrySet()) {
+                    Factory factoryTask = new Factory();
+
+                    List<String> argsList = new ArrayList<>();
+                    argsList.add(element.getKey());
+                    argsList.add(element.getValue());
+                    String idStr = String.valueOf(taskId + 1);
+                    argsList.add((idStr));
+
+                    Task downloadTask = factoryTask.createTask(CrawlTask.class, argsList);//tipul+lista de argumente
+
+                    threadPoolInstance.putTask(downloadTask);
+                }
+            }
+
+        }catch (InternalException error){
+            error.getMessage();
+        }catch (InputException error){
+            error.getMessage();
+        }
     }
-    private void replaceHTMLRef(){
 
+    /**
+     * Method that replaces all references with the names of locally downloaded files
+     *
+     */
+    private void replaceHTMLRef() {
+        try {
+
+            String inPath = targetPath + "\\" + url;//aici cum numim fisierele-> NU putem pune url ca nume
+            String outPath = targetPath + "\\1" + url;
+
+            File oldFile = new File(inPath);
+            PrintWriter newFile = new PrintWriter(outPath);
+
+            Scanner reader = new Scanner(oldFile);
+
+            int nrRef = 0;
+            while (reader.hasNextLine()) {
+                int status = 0;
+                String data = reader.nextLine();
+                String patternString = "\\b((https?|ftp):\\/\\/)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[A-Za-z]{2,6}\\b(\\/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)*(?:\\/|\\b)";
+                Pattern pattern = Pattern.compile(patternString);
+                Matcher matcher = pattern.matcher(data);
+
+                String newStateLine = null;
+                String lastStateLine = data;
+                while (matcher.find()) {
+                    status = 1;
+                    nrRef++;
+
+                    String stringValue = String.valueOf(nrRef);
+                    int lenString = lastStateLine.length();
+
+                    newStateLine = lastStateLine.substring(0, matcher.start()) + stringValue + lastStateLine.substring(matcher.end(), lenString);
+                    matcher = pattern.matcher(newStateLine);
+                    lastStateLine = newStateLine;
+                }
+                if (status != 0) {
+                    newFile.println(newStateLine);
+                } else {
+                    newFile.println(data);
+                }
+            }
+            newFile.close();
+            reader.close();
+            oldFile.delete();
+
+            /**
+             * The new file is renamed after the input file
+             */
+            Path source = Paths.get(outPath);
+            Files.move(source, source.resolveSibling(inPath));
+        } catch (Exception error){
+            error.getMessage();
+        }
     }
 
     /**
@@ -60,7 +179,9 @@ public class CrawlTask extends Task {
      */
     @Override
     protected void _run() {
-
+        startTasks(parsePageHtml());
+        downloadPage();
+        replaceHTMLRef();
 
     }
 }
