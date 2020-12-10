7e0119dfc8646f693513823ace6440fcfef114b9
vladteapa <georgehariga@yahoo.com>
2020-12-10 13:10:19 +0200

    :running: Made modifications to Configurations
    Issue: #48
    Configurations will not throw any exception and it will always have a
    configuration be it default, from a file, or a combination.
    
    Signed-off-by: vladteapa <georgehariga@yahoo.com>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/Configurations.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/Configurations.java
@@ -1,6 +1,9 @@
 package ro.mta.teamsubsonic.webcrawler.model;
 
-import ro.mta.teamsubsonic.webcrawler.model.exceptions.*;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.CrawlerException;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.FileException;
+import ro.mta.teamsubsonic.webcrawler.model.exceptions.InputException;
+import ro.mta.teamsubsonic.webcrawler.view.Logger;
 
 import java.io.File;
 import java.util.HashMap;
@@ -17,12 +20,20 @@ import java.util.Scanner;
 public class Configurations {
 
     /**
-     * Members of class description
-     */
+     * @param instance, is the singleton object
+     * @param delay, is the delay in ms between requests
+     * @param threadsNumber, number of threads used
+     * @param depthLevel, number of recursion levels in requests
+     * @param logLevel, how much to log
+     * @param logFile, the file to log in
+     * @param targetDirectory, the directory where to download the files
+     * @param configFileLocation, location of the configuration file
+     * @param robots, true if using robots.txt or false if not using
+     **/
 
     private static Configurations instance = null;
 
-    private double delay;
+    private long delay;
 
     private int threadsNumber;
     private int depthLevel;
@@ -32,41 +43,41 @@ public class Configurations {
     private String targetDirectory;
     private String configFileLocation;
 
+    private boolean robots;
+
+    private static Logger logger;
+
     private Configurations() {
+        logger = Logger.getInstance();
     }
 
     /**
-     * Function that returns the instance to Configurations (must not call this one to initialize)
+     * Function that returns the instance to Configurations with default configurations
      *
      * @return return reference to object
-     * @throws _CrawlerException
+     * @throws CrawlerException
      */
-    public static Configurations getInstance() throws _CrawlerException {
+    public static Configurations getInstance() {
         if (instance == null) {
-            throw new InternalException("Configurations not initialized! Call the other getInstance first!");
+            instance = new Configurations();
+            reconfigure(null);
         }
         return instance;
     }
 
     /**
-     * Function that initializez the configuration object. Must be called first (in case of reconfigurations call this function again)
+     * Function that initializez the configuration object with custom configuration
      *
      * @param configFileLocation it can be null for a default configuration
      * @return return the reference to object
-     * @throws _CrawlerException
+     * @throws CrawlerException
      */
 
-    public static Configurations getInstance(String configFileLocation) throws _CrawlerException {
-        try {
-            if (instance == null)
-                instance = new Configurations();
-            reconfigure(configFileLocation);
-            return instance;
-        } catch (InputException ex) {
-            throw new InputException("Bad config file");
-        } catch (Exception ex) {
-            throw new FileException("Bad file name");
-        }
+    public static Configurations getInstance(String configFileLocation) {
+        if (instance == null)
+            instance = new Configurations();
+        reconfigure(configFileLocation);
+        return instance;
     }
 
     /**
@@ -75,47 +86,61 @@ public class Configurations {
      * @param configFileLocation it can be null for a default configuration
      * @throws Exception which will be translated into another type of Exception
      */
-    private static void reconfigure(String configFileLocation) throws Exception {
+    private static void reconfigure(String configFileLocation) {
+        try {
+            instance.threadsNumber = 4;
+            instance.delay = 20;
+            instance.depthLevel = 4;
+            instance.logFile = "log.txt";
+            instance.logLevel = 3;
+            instance.targetDirectory = "out";
+            instance.configFileLocation = null;
+            instance.robots = true;
+            if (configFileLocation == null) {
+                configFileLocation = "config.cfg";
+            }
 
-        instance.threadsNumber = 4;
-        instance.delay = 20;
-        instance.depthLevel = 4;
-        instance.logFile = "log.txt";
-        instance.logLevel = 2;
-        instance.targetDirectory = "out";
-        instance.configFileLocation = null;
-        if (configFileLocation == null) {
-            return;
+            File file = new File(configFileLocation);
+            Scanner scanner = new Scanner(file);
+            String fileData = null;
+            HashMap<String, String> hashMap = new HashMap<>();
+
+
+            while (scanner.hasNextLine()) {
+                fileData = scanner.nextLine();
+                String[] fields = fileData.split("=");
+                if (fields.length != 2)
+                    throw new InputException("File is not properly formatted. Using default configuration!");
+                hashMap.put(fields[0], fields[1]);
+            }
+
+            if (hashMap.containsKey("threadsNumber"))
+                instance.threadsNumber = Integer.parseInt(hashMap.get("threadsNumber"));
+            if (hashMap.containsKey("delay"))
+                instance.delay = Long.parseLong(hashMap.get("delay"));
+            if (hashMap.containsKey("depthLevel"))
+                instance.depthLevel = Integer.parseInt(hashMap.get("depthLevel"));
+            if (hashMap.containsKey("logFile"))
+                instance.logFile = hashMap.get("logFile");
+            if (hashMap.containsKey("targetDirectory"))
+                instance.targetDirectory = hashMap.get("targetDirectory");
+            if (hashMap.containsKey("logLevel"))
+                instance.logLevel = Integer.parseInt(hashMap.get("logLevel"));
+            if (hashMap.containsKey("robots"))
+                instance.robots = Boolean.parseBoolean(hashMap.get("robots"));
+            instance.configFileLocation = configFileLocation;
+            logger.setOutputFile(instance.logFile);
+        } catch (InputException ex) {
+            ex.getMessage();
+        } catch (NumberFormatException ex) {
+            InputException exception = new InputException("Bad config file! Using rest of params with default value!");
+            exception.getMessage();
+        } catch (Exception ex) {
+            FileException exception = new FileException("File not found. Using default configuration!");
+            exception.getMessage();
+        } finally {
+            logger.write("Configurations done!", 2, 3);
         }
-
-        File file = new File(configFileLocation);
-        Scanner scanner = new Scanner(file);
-        String fileData = null;
-        HashMap<String, String> hashMap = new HashMap<>();
-
-
-        while (scanner.hasNextLine()) {
-            fileData = scanner.nextLine();
-            String[] fields = fileData.split("=");
-            if (fields.length != 2)
-                throw new InputException("");
-            hashMap.put(fields[0], fields[1]);
-        }
-
-        if (hashMap.containsKey("threadsNumber"))
-            instance.threadsNumber = Integer.parseInt(hashMap.get("threadsNumber"));
-        if (hashMap.containsKey("delay"))
-            instance.delay = Double.parseDouble(hashMap.get("delay"));
-        if (hashMap.containsKey("depthLevel"))
-            instance.depthLevel = Integer.parseInt(hashMap.get("depthLevel"));
-        if (hashMap.containsKey("logFile"))
-            instance.logFile = hashMap.get("logFile");
-        if (hashMap.containsKey("targetDirectory"))
-            instance.targetDirectory = hashMap.get("targetDirectory");
-        if (hashMap.containsKey("logLevel"))
-            instance.logLevel = Integer.parseInt(hashMap.get("logLevel"));
-        instance.configFileLocation = configFileLocation;
-
     }
 
     public int getThreadsNumber() {
@@ -126,11 +151,11 @@ public class Configurations {
         this.threadsNumber = threadsNumber;
     }
 
-    public double getDelay() {
+    public long getDelay() {
         return delay;
     }
 
-    public void setDelay(double delay) {
+    public void setDelay(long delay) {
         this.delay = delay;
     }
 
