64c493f219503147ce00afb52f8217d1900fea4f
Phineas09 <claudiu.ghenea15@yahoo.ro>
2020-12-12 16:40:00 +0200

    :sparkles: Finished robots functionality
    Issue: #60
    Am terminat functionalitatea de a scana robots.txt, adaugat log-uri si
    metoda de get pentru robots din clasa Configurations.
    
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

--- WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/Configurations.java
+++ WebCrawler/src/ro/mta/teamsubsonic/webcrawler/model/Configurations.java
@@ -155,6 +155,10 @@ public class Configurations {
         return delay;
     }
 
+    public boolean trackRobots() {
+        return robots;
+    }
+
     public void setDelay(long delay) {
         this.delay = delay;
     }
